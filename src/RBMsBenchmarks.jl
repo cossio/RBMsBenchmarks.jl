945dc7d1ba0ea610932b6a43e0944464b533d5f0
cossio <j.cossio.diaz@gmail.com>
2022-01-20 01:52:33 +0100

    DATADEPS_ALWAYS_ACCEPT

--- src/RBMsBenchmarks.jl
+++ src/RBMsBenchmarks.jl
@@ -9,6 +9,8 @@ import RestrictedBoltzmannMachines as RBMs
 # make sure we are using MKL, which is faster
 @show BLAS.get_config()
 
+ENV["DATADEPS_ALWAYS_ACCEPT"] = "true"
+
 include("util.jl")
 include("mnist.jl")
 
